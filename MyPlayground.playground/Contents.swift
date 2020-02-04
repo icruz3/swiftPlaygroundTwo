1e72d819e21b6797763e165eddf60e9ed8e6ee9e
icruz3 <59888011+icruz3@users.noreply.github.com>
2020-02-04 10:14:54 -1000

    Playground two coding mostly gradeA, if/else

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var goodGrade = "i've been doing all my work"
+
+if goodGrade == "i've been doing all my work"
+{
+    print ( "i shoudl be getting an A " )
+}
+
+else
+    
+{
+    
+print ( " i need to work harder in this class" )
+    
+}
+
+var gradeA = true
+    
+if gradeA
+
+{
+print ( "i really like this class")
+}
+
+else{
+    print("im not sure about coding")
+}
+    
+
